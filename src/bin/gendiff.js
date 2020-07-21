60b9059b8db8f29e03cd96508d8b8d56038d321c
alexander <oulfc93@gmail.com>
2020-07-22 01:34:12 +0300

    add commander's help

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,3 +1,14 @@
 #!/usr/bin/node
 
-console.log('Welcome to calculator difference!');
+const { Command } = require('commander');
+const pack = require('../../package.json');
+
+const program = new Command();
+
+program
+  .arguments('<firstConfig> <secondConfig>')
+  .version(pack.version)
+  .description(pack.description)
+  .option('-f, --format [type]', 'Output format');
+
+program.parse(process.argv);
