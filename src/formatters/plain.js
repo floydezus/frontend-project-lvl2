90fa097c842c5b1798f877760e60617ef569c714
alexander <oulfc93@gmail.com>
2020-10-20 21:51:29 +0300

    refactor plain formatter

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -6,27 +6,33 @@ const stringifyValue = (value) => {
   }
   return (_.isString(value)) ? `'${value}'` : `${value}`;
 };
+const verifyNameKey = (parentName, currentNodeName) => {
+  if (_.isEmpty(parentName)) {
+    return `${currentNodeName}`;
+  }
+  return `${parentName}.${currentNodeName}`;
+};
 const formatPlain = (diffTree) => {
-  const iter = (tree, parentName = '') => {
+  const iter = (node, parentName = '') => node.flatMap((tree) => {
     const { type } = tree;
+    const currentName = verifyNameKey(parentName, tree.name);
     switch (type) {
       case 'added':
-        return `Property '${parentName}.${tree.name}' was added with value: ${stringifyValue(tree.value2)}`;
+        return `Property '${currentName}' was added with value: ${stringifyValue(tree.value2)}`;
       case 'deleted':
-        return `Property '${parentName}.${tree.name}' was removed`;
+        return `Property '${currentName}' was removed`;
       case 'unchanged':
         return null;
       case 'changed':
-        return `Property '${parentName}.${tree.name}' was updated. From ${stringifyValue(tree.value1)} to ${stringifyValue(tree.value2)}`;
+        return `Property '${currentName}' was updated. From ${stringifyValue(tree.value1)} to ${stringifyValue(tree.value2)}`;
       case 'nested':
-        return tree.children.flatMap((node) => iter(node, `${parentName}.${tree.name}`));
+        return iter(tree.children, currentName);
       default:
         throw new Error(`Unknown type of node: '${type}'!`);
     }
-  };
+  });
 
-  const strings = diffTree
-    .flatMap((e) => iter(e, ''));
+  const strings = iter(diffTree, '');
 
   return strings
     .filter(_.identity)
